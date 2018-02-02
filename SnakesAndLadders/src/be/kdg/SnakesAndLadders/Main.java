ef8c2a7d3b394672ecc72873064347f9cceb998b
Unknown <marnix.wyns@gmail.com>
2018-02-02 14:55:08 +0100

    Initial Class and project Setup

---
+++ SnakesAndLadders/src/be/kdg/SnakesAndLadders/Main.java
@@ -0,0 +1,7 @@
+package be.kdg.SnakesAndLadders;/*
+ * Marnix Wyns
+ * 2/02/2018
+ */
+
+public class Main {
+}
---
+++ SnakesAndLadders/src/be/kdg/SnakesAndLadders/model/PieceColor.java
@@ -0,0 +1,7 @@
+package be.kdg.SnakesAndLadders.model;/*
+ * Marnix Wyns
+ * 2/02/2018
+ */
+
+public enum PieceColor {
+}
